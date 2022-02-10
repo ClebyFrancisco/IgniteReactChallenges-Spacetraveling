3ded4132d35cccacb0d868408057f470f9039c26
Cleby Francisco <clebysilva5@gmail.com>
2022-02-09 22:24:09 -0300

    Page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from 'next';
-
+import Header from '../components/Header';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Link from 'next/link';
 
 interface Post {
   uid?: string;
@@ -24,13 +26,50 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <main className={commonStyles.container}>
+      <Header />
+      <div className={styles.posts}>
+        <Link href="/">
+          <a className={styles.post}>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <ul>
+              <li>
+                <FiCalendar /> 15 Mar 2021
+              </li>
+              <li>
+                <FiUser />
+                Cleby Francisco
+              </li>
+            </ul>
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.post}>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <ul>
+              <li>
+                <FiCalendar /> 15 Mar 2021
+              </li>
+              <li>
+                <FiUser />
+                Cleby Francisco
+              </li>
+            </ul>
+          </a>
+        </Link>
+        <button type="button">Carregar mais posts</button>
+      </div>
+    </main>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+/*   // export const getStaticProps = async () => {
+  //   // const prismic = getPrismicClient();
+  //   // const postsResponse = await prismic.query(TODO);
 
-//   // TODO
-// };
+  //   // TODO
+} */
